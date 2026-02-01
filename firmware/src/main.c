7a963bf5afcc4ca392a5b7a8f8182171dfc9380e
Rafael Costa <rafael.arthur.costa@protonmail.com>
2026-02-01 16:51:31 -0500

    Add INVALID state and update function prototypes in main.c

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,18 +1,44 @@
 #include <stdio.h>
 
-enum states {BOOT, ARMED, DRILLING, FAULT, SHUTDOWN};
+enum states {BOOT, ARMED, DRILLING, FAULT, SHUTDOWN, INVALID};
 
 int arm(int state);
 int disarm(int state);
+int drill(int state);
 int fault(int state);
 int reset_fault(int state);
 
 int main() {
     enum current_state = BOOT;
 
-
+    current_state = arm(current_state);
+    current_state = arm(current_state);
+    current_state = arm(current_state);
+    current_state = arm(current_state);
 }
 
 int arm(int state) {
+    if (state != BOOT)
+        return INVALID;
     
+    return ARMED;
+}
+
+int drill(int state) {
+    if (state != ARMED)
+        return INVALID;
+    
+    return DRILLING;
+}
+
+int disarm(int state) {
+    return ARMED;
+}
+
+int fault(int state) {
+    return FAULT;
+}
+
+int reset_fault(int state) {
+    return ARMED;
 }
